7ce0af1fb322e77a90ee9254b7874efc176fd1cb
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-10-12 21:49:11 +0600

    done

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const colors = require("colors");
 const app = require("./app");
 
 // database connection
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mh2ii.mongodb.net/jobPortal?retryWrites=true&w=majority`
